d0f7319f88a05e4b89559137a1d95cddf8f1496c
Alexandra <126861307+Kobatoha@users.noreply.github.com>
2025-01-09 15:37:21 +0300

    lesson 35

--- main.py
+++ main.py
@@ -1316,3 +1316,44 @@ def lesson_34():
 
     print(type(A))
     A = type('Point', (), {"MAX_COORD": 100, "MIN_COORD": 0})
+
+
+def lesson_35():
+    """
+    Пользовательские метаклассы.
+    Две одинаковые записи. Обычное создание класса и динамическое создание класса.
+    class Point:                    ==      A = type('Point', (), {"MAX_COORD": 100, "MIN_COORD": 0})
+        MAX_COORD = 100
+        MIN_COORD = 0
+    """
+
+    # Простейшая функция-метакласс:
+    def create_class_point(name, base, attrs):
+        attrs.update({"MAX_COORD": 100, "MIX_COORD": 0})
+        return type(name, base, attrs)
+
+    class Point(metaclass=create_class_point):
+        def get_coords(self):
+            return 0, 0
+
+    pt = Point()
+    print(pt.MAX_COORD)
+    print(pt.get_coords())
+
+    class Meta(type):
+        def __new__(cls, name, base, attrs):
+            attrs.update({"MAX_COORD": 200, "MIX_COORD": 0})
+            return type.__new__(cls, name, base, attrs)
+
+        # def __init__(cls, name, base, attrs):
+        #     super().__init__(name, base, attrs)
+        #     cls.MAX_COORD = 200
+        #     cls.MIN_COORD = 0
+
+    class Point2(metaclass=Meta):
+        def get_coords(self):
+            return 0, 0
+
+    pt2 = Point2()
+    print(pt2.MAX_COORD)
+    print(pt2.get_coords())
